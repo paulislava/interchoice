24d7129408efad4a790aad8ce06edfe0e6ce2b34
Pavel Kondratov <p.kondratov@brusnika.ru>
2022-06-02 01:47:39 +0500

    logout right

--- frontend/src/components/pages/LogoutPage.tsx
+++ frontend/src/components/pages/LogoutPage.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
 import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { Loader } from 'components/Loader'
 import { useAuth } from 'root/hooks/store'
 import { logout } from 'root/store/user/user.actions'
+import { appRoutes } from 'root/appRoutes'
 
 export const LogoutPage: React.FC = () => {
   const authorized = useAuth()
   const dispatch = useDispatch()
+  const history = useHistory()
 
   React.useEffect(() => {
-    dispatch(logout.request())
+    if (authorized === null) {
+      history.push(appRoutes.login())
+    } else {
+      dispatch(logout.request())
+    }
   }, [authorized])
 
   return <Loader />
