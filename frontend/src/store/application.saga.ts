26f4850eedf00a3bcff35513d357a23d057b0603
Pavel Kondratov <p.kondratov@brusnika.ru>
2022-06-01 19:26:11 +0500

    user projects saga

--- frontend/src/store/application.saga.ts
+++ frontend/src/store/application.saga.ts
@@ -2,9 +2,10 @@ import { all, fork, Effect } from 'redux-saga/effects'
 
 import userSaga from './user/user.saga'
 import projectSaga from './movie/project/project.saga'
+import userProjectsSaga from './user-projects/user-projects.saga'
 import { versionInfo } from 'root/utils/version'
 
 export default function* rootSaga(): Generator<Effect, void, void> {
   console.info(versionInfo())
-  yield all([fork(userSaga), fork(projectSaga)])
+  yield all([fork(userSaga), fork(projectSaga), fork(userProjectsSaga)])
 }
